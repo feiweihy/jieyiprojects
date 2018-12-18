a4467e1f08a2813c0e9c540bc58734a771cbd0c7
unknown <529486618@qq.com>
2018-12-18 11:22:19 +0800

    第一次提交

---
+++ LYGYKT/LYGMaintainace/com/jieyi/qrcode/GetFilesFromUnionFTP.py
@@ -0,0 +1,50 @@
+'''
+从银联商户的FTP上获取对账文件
+'''
+
+import ftplib,datetime
+
+host = 'uisftp.chinaums.com'
+username = '10020035900000'
+password = 'Ab123456'
+
+ftp = ftplib.FTP(host)  # 实例化FTP对象
+ftp.login(username, password)  # 登录
+
+# 获取当前路径
+pwd_path = ftp.pwd()
+print("FTP当前路径:", pwd_path)
+
+# 更改到工作路径
+ftp.cwd("/build")
+pwd_path = ftp.pwd()
+print("FTP当前路径:", pwd_path)
+
+def getYesterday():
+    today=datetime.date.today()
+    oneday=datetime.timedelta(days=1)
+    yesterday=today-oneday
+    yesterdaystr = datetime.datetime.strftime(yesterday,"%Y-%m-%d")
+    yesterdaystrret = yesterdaystr.replace("-","")
+    return yesterdaystrret
+
+# print(getYesterday())
+
+files = ftp.nlst()
+print(files)
+filesNew = [x for x in files if '.txt' in x and getYesterday() in x]
+print(filesNew)
+
+def ftp_download(localpath,file):
+     '''以二进制形式下载文件'''
+     file_remote = file
+     file_local = localpath+file
+     bufsize = 1024  # 设置缓冲器大小
+     fp = open(file_local, 'wb')
+     ftp.retrbinary('RETR %s' % file_remote, fp.write, bufsize)
+     fp.close()
+
+for file in filesNew:
+    print(file)
+    ftp_download("E:\\111\\lygzhzf\\recv\\",file)
+
