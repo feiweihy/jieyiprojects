5102d7e6e1a328eba0696cf88a0e82fa4e4b2004
unknown <529486618@qq.com>
2019-03-22 09:45:53 +0800

    Signed-off-by: unknown <529486618@qq.com>

---
+++ JieYi/jieyi-tools/com/jieyi/TestUtil.py
@@ -0,0 +1,18 @@
+import com.jieyi.util.StringUtil as StringUtil
+
+if __name__ == '__main__':
+    # StringUtil测试
+    print(StringUtil.hex_str2bytes("313233"))
+    print(StringUtil.bytes2hex_str(StringUtil.hex_str2bytes("313233")))
+    print(StringUtil.add_left0("111", 10))
+    print(StringUtil.add_right0("111", 10))
+    print(StringUtil.add_right_space("111", 10))
+    print(StringUtil.add_str_as_some_multiple("111", 0, 10, 'F'))
+    print(StringUtil.add_str_as_some_multiple("111", 1, 20, 'F'))
+    print(StringUtil.get_random_str_according_datetime(32, 0))
+    print(StringUtil.get_random_str_according_datetime(32, 1))
+    print(StringUtil.add_right_space_for_bytes('', 'GBK', 10))
+    print(StringUtil.add_right_space_for_bytes('牛', 'GBK', 10))
+    print(StringUtil.reverse_str('112233445566'))
+    print(StringUtil.reverse_str('11223344556'))
+    print(StringUtil.str_xor_str('112233445566', '665544332211'))
--- JieYi/jieyi-tools/com/jieyi/util/StringUtil.py
+++ JieYi/jieyi-tools/com/jieyi/util/StringUtil.py
@@ -95,20 +95,3 @@ def str_xor_str(str1, str2):
         bytes_new += bytes([xor_result])
     return_str = bytes2hex_str(bytes_new)
     return return_str
-
-
-if __name__ == '__main__':
-    print(hex_str2bytes("313233"))
-    print(bytes2hex_str(hex_str2bytes("313233")))
-    print(add_left0("111", 10))
-    print(add_right0("111", 10))
-    print(add_right_space("111", 10))
-    print(add_str_as_some_multiple("111", 0, 10, 'F'))
-    print(add_str_as_some_multiple("111", 1, 20, 'F'))
-    print(get_random_str_according_datetime(32, 0))
-    print(get_random_str_according_datetime(32, 1))
-    print(add_right_space_for_bytes('', 'GBK', 10))
-    print(add_right_space_for_bytes('牛', 'GBK', 10))
-    print(reverse_str('112233445566'))
-    print(reverse_str('11223344556'))
-    print(str_xor_str('112233445566', '665544332211'))
